1adc8a8f3b76905d0794e3c9742a9c98a6eb0244
Cheehin Choa <Cheehin.Choa@aryza.com>
2024-08-21 16:45:18 +0100

    seperate_file compatitle with path object

--- backend/services/organizing.py
+++ backend/services/organizing.py
@@ -1,7 +1,8 @@
 #Logic for organizing files based on metadata.
 import os
+from backend.utils.file_ops import scan_dir
 
-def scan_files(files):
+def seperate_files(files):
     ''' Seperate files based on their type '''
     photos = []
     editedphotos = []
@@ -14,14 +15,14 @@ def scan_files(files):
     metadata_ext = ['.json', '.xml', '.csv']
 
     for file in files:
-        if file.endswith(tuple(photo_ext)):
-            photos.append(file)
-        elif file.endswith(tuple(edited_ext)):
+        file_str = str(file)
+        if file_str.lower().endswith(tuple(edited_ext)):
             editedphotos.append(file)
-        elif file.endswith(tuple(video_ext)):
+        elif file_str.lower().endswith(tuple(photo_ext)):
+            photos.append(file)
+        elif file_str.lower().endswith(tuple(video_ext)):
             videos.append(file)
-        elif file.endswith(tuple(metadata_ext)):
+        elif file_str.lower().endswith(tuple(metadata_ext)):
             metadata.append(file)
     
     return photos, editedphotos, videos, metadata
-
